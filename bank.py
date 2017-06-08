4d6b60e1b58ef3c4e33d455e3072b87bcd301c91
Hyang Min Jeong <blindfish@Hyang-ui-iMac.local>
2017-06-09 00:30:38 +0900

    start

---
+++ bank.py
@@ -0,0 +1,21 @@
+import numpy as np
+import sklearn.cluster as clust
+
+f = open("/Users/blindfish/Downloads/dataset.tsv")
+
+data = []
+
+for i in f.read().split("\n"):
+    data.append(i.split("\t"))
+
+cdata = []
+for i in data:
+    cdata.append(i[0:8])
+
+trainData = np.array()
+
+hclust = clust.AgglomerativeClustering(n_clusters=4)
+#kclust = clust.k_means(n_clusters = 4, init='k-means++', precompute_distances= 'auto')
+kclust = clust.KMeans(n_clusters = 4, random_state=0).fit(cdata)
+
+
