9799f26ab52596b14b5068a3aca155b3776e4cc4
Hyang Min Jeong <blindfish@Hyang-ui-iMac.local>
2017-06-12 01:31:47 +0900

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # bank
+
+원래는 python보다 R이 더 익숙하기 때문에 사용하는데 어려움이 있었음
+결과적으로 R이 python보다 더 사용이 쉽고 빠르다는 것이 결론
+
+데이터 전처리를 하는대 대부분의 시간을 사용함 
+numpy와 sklearn에 전처리하는 모듈들도 있다는 것을 알게됨
+
+categorical 데이터를 컬럼의 0,1의 값으로 변환하여 보다 알고리즘의 정확도를 높임
+
+ex)[single] ->  married, divorced, single 
+                    0        0        1
+
+countineous 데이터는 min, max 기준으로 0~1사이 값으로 만듬
+
+ex) (x - min)/(max - min)
+
+hierarchy clustering을 통해서 대체적으로 몇개의 그룹으로 나뉘는지 보려했지만 메모리를 많이 써서 실패함
+일단 kmean을 통해서 적당히 여러개의 k값을 정한후에 clustering을 시도해보고 적당한 것을 선정 
+
+어느정도 well clust됬는지에 대한 결과를 알고 싶었지만 추후에 따로 cluster center와 데이터들 사이에 거리값의 평균이 최소가 되는 k를 선택하는것이 적달하다고 생각함 (여기까지는 구현하지 못함)
+
+다시 나머지 변수들을 category와 countineous로 나누어 데이터를 전처리하고 모델을 만들어서 학습함 
+
+사용한것은 decision tree, random forest, extra tree(정확한 알고리즘은 모르겠음)임
+
+위 알고리즘을 선택한 이유는 decision tree류의 알고리즘이 category와 countineous로 복합된 데이터를 다루기에 가장 적합하기 때문
+
+decision tree의 경우 0.65의 score가 나왔고 시험적으로 해보았음
+
+앙상블기법을 사용한 다른 알고리즘이 더 잘나옴
+
+random forest의 경우에는 모델의 수를 늘려서 vote하는 경우 10에서 50까지는 성능이 향상되나 그이상에서는 많이 향상되지 않음 100으로 하였을경우 0.83을 기록함 
+
+extra tree의 경우에는 50과 100에서 큰차이 없이 대략 0.8이 나옴 
+
+cross_val_score란 함수를 의심 없이 사용하였지만, train 데이터와 test데이터를 나눠서 모델의 성능을 측정해주는 함수
+
+시간이 더 된다면 Gradient Boosting이나 Ada boost도 자세히 해보고 싶은 아쉬움이 남음 
+
--- bank.py
+++ bank.py
@@ -1,6 +1,11 @@
 import numpy as np
 import sklearn.cluster as clust
 import sklearn.preprocessing as prepro
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
 
 f = open("/Users/blindfish/bank/dataset.tsv")
 
@@ -49,6 +54,15 @@ v6 = min_max_scaler.fit_transform(get_ncol_float(cdata,5))
 v7 = category2vec(get_ncol(cdata,6))
 v8 = category2vec(get_ncol(cdata,7))
 
+v9 = category2vec(get_ncol(data,8))
+v10 = min_max_scaler.fit_transform(get_ncol_float(data,9))
+v11 = category2vec(get_ncol(data,10))
+v12 = min_max_scaler.fit_transform(get_ncol_float(data,11))
+v13 = min_max_scaler.fit_transform(get_ncol_float(data,12))
+v14 = min_max_scaler.fit_transform(get_ncol_float(data,13))
+v15 = category2vec(get_ncol(data,14))
+y = category2vec(get_ncol(data,15))
+
 trainData = []
 for i in range(len(cdata)):
     trainpart=[]
@@ -62,11 +76,64 @@ for i in range(len(cdata)):
     trainpart = trainpart+v8[i].tolist()
     trainData.append(trainpart)
     #trainData.append([np.array(v1[i]),v2[i],v3[i],v4[i],v5[i],np.array(v6[i]),v7[i],v8[i]])
+
 trainData = np.array(trainData)
 print trainData[0]
 
 #hclust = clust.AgglomerativeClustering(n_clusters=4)
+# not enough memory
+#clustresult = hclust.fit(trainData)
 #kclust = clust.k_means(n_clusters = 4, init='k-means++', precompute_distances= 'auto')
-#kclust = clust.KMeans(n_clusters = 4, random_state=0).fit(cdata)
+kclust3 = clust.KMeans(n_clusters = 3, random_state=0).fit(trainData)
+kclust4 = clust.KMeans(n_clusters = 4, random_state=0).fit(trainData)
+kclust5 = clust.KMeans(n_clusters = 5, random_state=0).fit(trainData)
+kclust6 = clust.KMeans(n_clusters = 6, random_state=0).fit(trainData)
 
+print kclust4.cluster_centers_
+
+trainData = []
+for i in range(len(cdata)):
+    trainpart=[]
+    trainpart.append(v1[i])
+    trainpart = trainpart+v2[i].tolist()
+    trainpart = trainpart+v3[i].tolist()
+    trainpart = trainpart+v4[i].tolist()
+    trainpart = trainpart+v5[i].tolist()
+    trainpart.append(v6[i])
+    trainpart = trainpart+v7[i].tolist()
+    trainpart = trainpart+v8[i].tolist()
+    trainpart = trainpart+v9[i].tolist()
+    trainpart.append(v10[i])
+    trainpart = trainpart+v11[i].tolist()
+    trainpart.append(v12[i])
+    trainpart.append(v13[i])
+    trainpart.append(v14[i])
+    trainpart = trainpart+v15[i].tolist()
+    trainData.append(trainpart)
+
+trainData = np.array(trainData)
+X = trainData
+dctree = DecisionTreeClassifier(max_depth=None, min_samples_split=2, random_state=0)
+rf = RandomForestClassifier(n_estimators=100, criterion="gini", max_depth=None, min_samples_split=2, random_state=0)
+extree = ExtraTreesClassifier(n_estimators=100, max_depth=None, min_samples_split=2, random_state=0)
+
+scores = cross_val_score(dctree, X, y)
+print "decision tree result"
+print scores.mean()
+#dtree estmater = 10  ---> 0.65
+scores = cross_val_score(rf, X, y)
+print "random forest tree result"
+print scores.mean()
+#rf estmater = 10  ---> 0.75
+#rf estmater = 50  ---> 0.82
+#rf estmater = 100  ---> 0.83
+scores = cross_val_score(extree, X, y)
+print "extra tree result"
+print scores.mean()
+#ext estmater = 50  ---> 0.8
+#ext estmater = 100  ---> 0.8
+
+ada = AdaBoostClassifier(n_estimators=100)
+scores = cross_val_score(ada, X, y)
+print scores.mean()
 
